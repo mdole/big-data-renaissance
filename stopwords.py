01c062757b4f22af7bf329848bb510afcc343c91
ChrisGallo <chrismgallo1@gmail.com>
2014-04-12 22:57:14 -0500

    list of stopwords

---
+++ stopwords.py
@@ -0,0 +1,6 @@
+stop_words = ["her", "she", "his", "him", "we", "them", "our", 
+	      "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", 
+	      "chapter", "chap", "book", 
+	      "are", "were", "is", "was",
+	      "so", "and",
+	      "haue", "vnto", "very"]
