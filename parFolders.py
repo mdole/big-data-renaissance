eec0094cc768b1e3b9a8dfa531bb7c7e6e8af549
Christopher M. Gallo <gallochr1@math.grinnell.edu>
2014-04-12 19:14:58 -0500

    Fixed a bug that only allowed you to run the program in the top_dir folder

--- parFolders.py
+++ parFolders.py
@@ -9,12 +9,12 @@ def make_subfolders(top_dir):
     for name in directs:                
 	if not os.path.exists(top_dir + "/%s" %name): #makes a new directory from the .txt name if one doesn't exist
 	    os.mkdir(top_dir + "/%s" %name)
-        text = open("%s.txt" %name).read()          #reads in the .txt file and splits it by paragraph
+        text = open(top_dir + "%s.txt" %name).read()          #reads in the .txt file and splits it by paragraph
 	if len(text) > 0:
 	    docs = text.split("%EOS%")
 	else:                                #if it has no text we skip to avoid erroring out
 	    continue
 	for i in range(0, len(docs)):             #write the file in the new subfolder
-	    file = open("%(name)s/%(name)s%(number)d.txt" %{"name": name, "number": i}, "w")
+	    file = open(top_dir + "%(name)s/%(name)s%(number)d.txt" %{"name": name, "number": i}, "w")
 	    file.write(docs[i])
 	    file.close()
